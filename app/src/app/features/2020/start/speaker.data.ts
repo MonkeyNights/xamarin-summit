8cdb08d8742c4061f4ddd76490f1771f66138770
William S Rodriguez <williamsrz@outlook.com>
2020-07-28 09:07:16 -0300

    update speakers

--- app/src/app/features/2020/start/speaker.data.ts
+++ app/src/app/features/2020/start/speaker.data.ts
@@ -97,5 +97,109 @@ export const TALKS20 = [
             ]
         }
     }
-
+    ,
+    {
+        title: '',
+        tags:[],
+        description: '',
+        speaker: {
+            name: 'Brandon Minnick',
+            avatar: '../../../assets/img/speakers/brandon.png',
+            bio: 'I am on the Developer Advocate team at Microsoft, and I love C#, .NET, and Mobile App development.  I worked at Xamarin (before the Microsoft acquisition) and continue to share my passions with fellow Xamarin, C# and Cloud developers.  I am an avid coder and have developed countless apps. I love working closely with the developer community, helping fellow mobile app and cloud developers make 5-star apps.',
+            position: 'Developer Advocate @ Microsoft',
+            social: [
+                {
+                    url: 'https://twitter.com/TheCodeTraveler',
+                    type: 'twitter'
+                },
+                {
+                    url: 'https://github.com/brminnick',
+                    type: 'github'
+                },
+                {
+                    url: 'https://www.linkedin.com/in/brandon-minnick/',
+                    type: 'linkedin'
+                }
+            ]
+        }
+    }
+    ,
+    {
+        title: '',
+        tags:[],
+        description: '',
+        speaker: {
+            name: 'Thiago Bertuzzi',
+            avatar: '../../../assets/img/speakers/bertuzzi.png',
+            bio: 'Arquiteto .Net e Lider técnico na NESS, trabalho com .net há 13 anos participando de projetos Desktop,Services, Web e agora Mobile utilizando Xamarin.',
+            position: 'Arquiteto .Net e Lider técnico @ NESS',
+            social: [
+                {
+                    url: 'https://twitter.com/tbertuzzi',
+                    type: 'twitter'
+                },
+                {
+                    url: 'https://github.com/tbertuzzi',
+                    type: 'github'
+                },
+                {
+                    url: 'https://www.linkedin.com/in/thiago-bertuzzi-74ab2a21/',
+                    type: 'linkedin'
+                }
+                ,
+                {
+                    url: 'https://www.facebook.com/thiago.bertuzzi',
+                    type: 'facebook'
+                }
+            ]
+        }
+    }
+    ,
+    {
+        title: '',
+        tags:[],
+        description: '',
+        speaker: {
+            name: 'Alejandro Ruiz',
+            avatar: '../../../assets/img/speakers/alejandro.jpg',
+            bio: "I'm a very experienced mobile app developer and architect using Xamarin framework, I have been working over 8+ years helping companies to ship 5 stars apps around the globe, currently I'm a Microsoft MVP: Developer Technologies and I'm helping communities and companies to improve his skills on mobile development and converting ideas into awesome experiences also I was Xamarin Certified Mobile Developer from 2013 to 2019 & I got awarded with the Xamarin MVP on 2017.",
+            position: 'Mobile Developer @ ArcTouch',
+            social: [
+                {
+                    url: 'https://twitter.com/alejandroruizva',
+                    type: 'twitter'
+                },
+                {
+                    url: 'https://github.com/alejandroruiz',
+                    type: 'github'
+                },
+                {
+                    url: 'https://www.linkedin.com/in/alejandro-ruiz-533a0a88/',
+                    type: 'linkedin'
+                }
+            ]
+        }
+    }
+    ,
+    {
+        title: '',
+        tags:[],
+        description: '',
+        speaker: {
+            name: 'Yan Ariel Fernandes de Souza',
+            avatar: '../../../assets/img/speakers/yan.png',
+            bio: "Atuando a 9 anos em produtos de tecnologia e atualmente ajudando o brasileiro a investir melhor em renda variável",
+            position: 'Product Manager @ XP Inc.',
+            social: [
+                // {
+                //     url: 'https://github.com/renantoniolo?tab=repositories',
+                //     type: 'github'
+                // },
+                {
+                    url: 'https://www.linkedin.com/in/yanfernandes130593/',
+                    type: 'linkedin'
+                }
+            ]
+        }
+    }
 ]
